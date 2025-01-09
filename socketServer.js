ea82a45bfc1ce3bac4e20f03f3ddb26e703c8fc1
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2025-01-09 16:11:12 +0700

    update .env

--- socketServer.js
+++ socketServer.js
@@ -3,7 +3,7 @@ const { Server } = require("socket.io");
 module.exports = (server) => {
     const io = new Server(server, {
         cors: {
-            origin: "http://localhost:3000", // Địa chỉ frontend
+            origin: process.env.FRONTEND, // Địa chỉ frontend
             methods: ["GET", "POST"],
         },
     });
