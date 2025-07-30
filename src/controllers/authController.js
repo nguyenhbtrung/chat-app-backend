4039c6b4f1c18af7bdc1637382f62a393c24cfd6
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2025-07-30 12:19:22 +0700

    use express-async-handler

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,23 +2,15 @@ import { loginAsync, registerAsync } from "../services/authService.js";
 
 export const register = async (req, res, next) => {
     const { userName, password, email } = req.body;
-    try {
-        await registerAsync({ userName, email, password });
-        res.status(201).json({ message: 'User registered successfully' });
-    } catch (error) {
-        next(error);
-    }
+    await registerAsync({ userName, email, password });
+    res.status(201).json({ message: 'User registered successfully' });
 };
 
 export const login = async (req, res, next) => {
     const { userName, password } = req.body;
-    try {
-        const result = await loginAsync({ userName, password });
-        res.status(200).json({
-            message: 'Login successfully',
-            data: result
-        });
-    } catch (error) {
-        next(error);
-    }
+    const result = await loginAsync({ userName, password });
+    res.status(200).json({
+        message: 'Login successfully',
+        data: result
+    });
 }
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,10 +2,11 @@ import { Router } from "express";
 import { login, register } from "../controllers/authController.js";
 import { validate } from "../middleware/validate.js";
 import { loginSchema, registerSchema } from "chat-app-zod-schema";
+import expressAsyncHandler from "express-async-handler";
 
 const router = Router();
 
-router.post("/register", validate(registerSchema), register);
-router.post("/login", validate(loginSchema), login);
+router.post("/register", validate(registerSchema), expressAsyncHandler(register));
+router.post("/login", validate(loginSchema), expressAsyncHandler(login));
 
 export default router;
