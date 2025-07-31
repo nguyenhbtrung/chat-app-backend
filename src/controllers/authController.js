30510d7142b54d8208b7d59c8cc551f339f5d0e7
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2025-07-31 13:28:21 +0700

    modify authController: edit response

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,14 +3,15 @@ import { loginAsync, registerAsync } from "../services/authService.js";
 export const register = async (req, res, next) => {
     const { userName, password, email } = req.body;
     await registerAsync({ userName, email, password });
-    res.status(201).json({ message: 'User registered successfully' });
+    res.status(201).json({
+        data: {
+            message: 'User registered successfully'
+        }
+    });
 };
 
 export const login = async (req, res, next) => {
     const { userName, password } = req.body;
-    const result = await loginAsync({ userName, password });
-    res.status(200).json({
-        message: 'Login successfully',
-        data: result
-    });
+    const data = await loginAsync({ userName, password });
+    res.status(200).json({ data });
 }
