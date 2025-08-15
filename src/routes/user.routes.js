8a640f858a6926c9067a30615eb6d837f365ccc8
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2025-08-15 13:15:18 +0700

    add getAllNonFriendUsers api, modify getNonFriendOnlineUser endpoint

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { requireAuth } from "../middleware/auth.js";
 import expressAsyncHandler from "express-async-handler";
-import { getNonFriendOnlineUsers } from "../controllers/user.controller.js";
+import { getAllNonFriendUsers, getNonFriendOnlineUsers } from "../controllers/user.controller.js";
 
 const router = Router();
 router.use(requireAuth);
 
-router.get('/online/non-friends', expressAsyncHandler(getNonFriendOnlineUsers));
+router.get('/non-friends', expressAsyncHandler(getAllNonFriendUsers));
+router.get('/non-friends/online', expressAsyncHandler(getNonFriendOnlineUsers));
 
 export default router;
