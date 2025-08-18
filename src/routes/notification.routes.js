3afa7c5317c975725e1cdd4e1426d4334bc89da4
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2025-08-19 03:52:29 +0700

    add getNotifications API

---
+++ src/routes/notification.routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { requireAuth } from "../middleware/auth.js";
+import expressAsyncHandler from "express-async-handler";
+import { getNotifications } from "../controllers/notification.controller.js";
+
+const router = Router();
+router.use(requireAuth);
+
+router.get('/non-friends', expressAsyncHandler(getNotifications));
+
+export default router;
